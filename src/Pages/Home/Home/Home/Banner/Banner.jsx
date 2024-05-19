94d6380a8b828b695d08f45e3d685522248e582b
Akas_Datta <012akasdatta@gmail.com>
2024-05-20 02:20:29 +0600

    background image added

--- src/Pages/Home/Home/Home/Banner/Banner.jsx
+++ src/Pages/Home/Home/Home/Banner/Banner.jsx
@@ -1,8 +1,12 @@
+import BannerDown from "./BannerDown/BannerDown";
 
 const Banner = () => {
     return (
-        <div className="text-center pt-6 md:pt-20 lg:pt-24">
-            <h2 className="text-2xl md:text-5xl lg:text-6xl font-bold text-gray-800">Ask questions <br /> from <span className="text-blue-500">any document</span></h2>
+        <div>
+            <div className="text-center pt-6 md:pt-20 lg:pt-24">
+                <h2 className="text-2xl md:text-5xl lg:text-6xl font-bold text-gray-800">Ask questions <br /></h2>
+            </div>
+            <BannerDown></BannerDown>
         </div>
     );
 };
---
+++ src/Pages/Home/Home/Home/Banner/BannerDown/BannerDown.jsx
@@ -0,0 +1,18 @@
+import './BannerDown.css'
+const BannerDown = () => {
+    return (
+        <div className="bg-container text-center rounded-lg">
+            <div>
+                <h2 className="text-2xl md:text-5xl lg:text-6xl font-bold text-gray-800"> from <span className="text-blue-500">any document</span></h2>
+                <p className="py-4 md:py-8 lg:py-10 text-xs md:text-2xl lg:text-2xl max-w-screen-sm mx-auto text-[#344054] font-semibold">
+                    Upload your PDF or DOC files and get instant answers to your questions with our AI-powered system
+                </p>
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default BannerDown;
