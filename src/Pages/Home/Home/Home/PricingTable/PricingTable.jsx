abfc9a15d29704cda625fdaeca4395b584b9c400
Akas_Datta <012akasdatta@gmail.com>
2024-05-21 22:22:06 +0600

    basic tabvle added

--- src/Pages/Home/Home/Home/PricingTable/PricingTable.jsx
+++ src/Pages/Home/Home/Home/PricingTable/PricingTable.jsx
@@ -1,13 +1,226 @@
+import { FaArrowRight, FaRegCheckCircle } from "react-icons/fa";
 
 const PricingTable = () => {
+
     return (
         <div className="bg-white px-5 max-w-screen-2xl mx-auto lg:px-10 overflow-x-hidden py-10 md:py-20 lg:py-24 text-center">
-            <h2 className=" text-2xl md:text-5xl lg:text-6xl font-bold text-gray-800">
-            Choose Your Plan And Start <br />Your Free <span className="text-blue-500">7-Day Trial</span>
+            <h2 className="text-2xl md:text-5xl lg:text-6xl font-bold text-gray-800">
+                Choose Your Plan And Start <br />Your Free <span className="text-blue-500">7-Day Trial</span>
             </h2>
-            <p className="py-4 md:py-8 lg:py-10 text-xs md:text-2xl lg:text-2xl max-w-screen-sm mx-auto text-[#344054] font-semibold">
+            <p className="py-4 md:py-8 lg:pt-10 lg:pb-0 text-xs md:text-2xl lg:text-2xl max-w-screen-sm mx-auto text-[#344054] font-semibold">
                 Save up to $5,468/year with the ‘Ask AI’ plan
             </p>
+            <div className='flex justify-center gap-8 text-xl'>
+                <button className=' font-bold p-4 text-[#344054] focus:text-black focus:border-black focus:border-b-2'>Yearly Discount</button>
+                <button className='font-bold p-4 text-[#344054] focus:text-black  focus:border-black focus:border-b-2'>Monthly Plan</button>
+            </div>
+
+            <div className="">
+                <section>
+                    <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+                        <div className="space-y-8 lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-4 lg:space-y-0">
+                            {/* <!-- Pricing Card --> */}
+                            <div className="flex flex-col mx-auto max-w-lg text-center text-gray-900 bg-white rounded-2xl border border-blue-100 shadow">
+                                <div className="bg-gray-50 xl:p-8 p-6 rounded-t-2xl">
+                                    <h3 className="mb-4 text-2xl font-bold">Basics</h3>
+                                    <h3 className="mb-4 text-lg text-gray-500 font-bold line-through">Normally $99/mo.</h3>
+
+                                    <h3 className="mb-4 text-xl font-bold">$82.5/ Month</h3>
+                                    <h3 className="mb-4 text-xl font-bold"><span className="text-gray-500">Billed annually</span> (Save $198/year)</h3>
+
+                                    <a href="#" className="text-white bg-blue-400 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center flex gap-2 justify-center">Start Free Trial <FaArrowRight className="mt-1"/></a>
+                                </div>
+
+                                <div className="xl:p-8 p-6 rounded-t-2xl">
+                                    
+                                    {/* <!-- List --> */}
+                                    <ul role="list" className="mb-8 space-y-4 text-left">
+                                        <li className="flex items-center space-x-3">
+                                            {/* <!-- Icon --> */}
+                                            <FaRegCheckCircle className="text-green-600 text-2xl"/>
+                                            <span>2 FREE content pages</span>
+                                        </li>
+                                        <li className="flex items-center space-x-3">
+                                            {/* <!-- Icon --> */}
+                                            <FaRegCheckCircle className="text-green-600 text-2xl"/>
+                                            <span>20 new content pages per month</span>
+                                        </li>
+                                        <li className="flex items-center space-x-3">
+                                            {/* <!-- Icon --> */}
+                                            <FaRegCheckCircle className="text-green-600 text-2xl"/>
+                                            <span>Personalized to your business</span>
+                                        </li>
+                                        <li className="flex items-center space-x-3">
+                                            {/* <!-- Icon --> */}
+                                            <FaRegCheckCircle className="text-green-600 text-2xl"/>
+                                            <span>High Quality Mixed Content</span>
+                                        </li>
+                                        <li className="flex items-center space-x-3">
+                                            {/* <!-- Icon --> */}
+                                            <FaRegCheckCircle className="text-green-600 text-2xl"/>
+                                            <span>SEO Optimized</span>
+                                        </li>
+                                        <li className="flex items-center space-x-3">
+                                            {/* <!-- Icon --> */}
+                                            <FaRegCheckCircle className="text-green-600 text-2xl"/>
+                                            <span>​Instant Indexing with Google & Bing</span>
+                                        </li>
+                                        <li className="flex items-center space-x-3">
+                                            {/* <!-- Icon --> */}
+                                            <FaRegCheckCircle className="text-green-600 text-2xl"/>
+                                            <span>Full Editing of all Blogs</span>
+                                        </li>
+                                        <li className="flex items-center space-x-3">
+                                            {/* <!-- Icon --> */}
+                                            <FaRegCheckCircle className="text-green-600 text-2xl"/>
+                                            <span>Internal Page Links</span>
+                                        </li>
+                                        <li className="flex items-center space-x-3">
+                                            {/* <!-- Icon --> */}
+                                            <FaRegCheckCircle className="text-green-600 text-2xl"/>
+                                            <span>​Call to Action (CTA) to any URL</span>
+                                        </li>
+                                    </ul>
+                                    <a href="#" className="text-white bg-blue-400 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center flex gap-2 justify-center">Start Free Trial <FaArrowRight className="mt-1"/></a>
+                                </div>
+                       
+                            </div>
+                            {/* <!-- Pricing Card --> */}
+                            <div className="flex flex-col mx-auto max-w-lg text-center text-gray-900 bg-white rounded-2xl border border-blue-100 shadow">
+                                <div className="bg-gray-50 xl:p-8 p-6 rounded-t-2xl">
+                                    <h3 className="mb-4 text-2xl font-bold">Basics</h3>
+                                    <h3 className="mb-4 text-lg text-gray-500 font-bold line-through">Normally $99/mo.</h3>
+
+                                    <h3 className="mb-4 text-xl font-bold">$82.5/ Month</h3>
+                                    <h3 className="mb-4 text-xl font-bold"><span className="text-gray-500">Billed annually</span> (Save $198/year)</h3>
+
+                                    <a href="#" className="text-white bg-blue-400 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center flex gap-2 justify-center">Start Free Trial <FaArrowRight className="mt-1"/></a>
+                                </div>
+
+                                <div className="xl:p-8 p-6 rounded-t-2xl">
+                                    
+                                    {/* <!-- List --> */}
+                                    <ul role="list" className="mb-8 space-y-4 text-left">
+                                        <li className="flex items-center space-x-3">
+                                            {/* <!-- Icon --> */}
+                                            <FaRegCheckCircle className="text-green-600 text-2xl"/>
+                                            <span>2 FREE content pages</span>
+                                        </li>
+                                        <li className="flex items-center space-x-3">
+                                            {/* <!-- Icon --> */}
+                                            <FaRegCheckCircle className="text-green-600 text-2xl"/>
+                                            <span>20 new content pages per month</span>
+                                        </li>
+                                        <li className="flex items-center space-x-3">
+                                            {/* <!-- Icon --> */}
+                                            <FaRegCheckCircle className="text-green-600 text-2xl"/>
+                                            <span>Personalized to your business</span>
+                                        </li>
+                                        <li className="flex items-center space-x-3">
+                                            {/* <!-- Icon --> */}
+                                            <FaRegCheckCircle className="text-green-600 text-2xl"/>
+                                            <span>High Quality Mixed Content</span>
+                                        </li>
+                                        <li className="flex items-center space-x-3">
+                                            {/* <!-- Icon --> */}
+                                            <FaRegCheckCircle className="text-green-600 text-2xl"/>
+                                            <span>SEO Optimized</span>
+                                        </li>
+                                        <li className="flex items-center space-x-3">
+                                            {/* <!-- Icon --> */}
+                                            <FaRegCheckCircle className="text-green-600 text-2xl"/>
+                                            <span>​Instant Indexing with Google & Bing</span>
+                                        </li>
+                                        <li className="flex items-center space-x-3">
+                                            {/* <!-- Icon --> */}
+                                            <FaRegCheckCircle className="text-green-600 text-2xl"/>
+                                            <span>Full Editing of all Blogs</span>
+                                        </li>
+                                        <li className="flex items-center space-x-3">
+                                            {/* <!-- Icon --> */}
+                                            <FaRegCheckCircle className="text-green-600 text-2xl"/>
+                                            <span>Internal Page Links</span>
+                                        </li>
+                                        <li className="flex items-center space-x-3">
+                                            {/* <!-- Icon --> */}
+                                            <FaRegCheckCircle className="text-green-600 text-2xl"/>
+                                            <span>​Call to Action (CTA) to any URL</span>
+                                        </li>
+                                    </ul>
+                                    <a href="#" className="text-white bg-blue-400 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center flex gap-2 justify-center">Start Free Trial <FaArrowRight className="mt-1"/></a>
+                                </div>
+                       
+                            </div>
+                            {/* <!-- Pricing Card --> */}
+                            <div className="flex flex-col mx-auto max-w-lg text-center text-gray-900 bg-white rounded-2xl border border-blue-100 shadow">
+                                <div className="bg-gray-50 xl:p-8 p-6 rounded-t-2xl">
+                                    <h3 className="mb-4 text-2xl font-bold">Basics</h3>
+                                    <h3 className="mb-4 text-lg text-gray-500 font-bold line-through">Normally $99/mo.</h3>
+
+                                    <h3 className="mb-4 text-xl font-bold">$82.5/ Month</h3>
+                                    <h3 className="mb-4 text-xl font-bold"><span className="text-gray-500">Billed annually</span> (Save $198/year)</h3>
+
+                                    <a href="#" className="text-white bg-blue-400 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center flex gap-2 justify-center">Start Free Trial <FaArrowRight className="mt-1"/></a>
+                                </div>
+
+                                <div className="xl:p-8 p-6 rounded-t-2xl">
+                                    
+                                    {/* <!-- List --> */}
+                                    <ul role="list" className="mb-8 space-y-4 text-left">
+                                        <li className="flex items-center space-x-3">
+                                            {/* <!-- Icon --> */}
+                                            <FaRegCheckCircle className="text-green-600 text-2xl"/>
+                                            <span>2 FREE content pages</span>
+                                        </li>
+                                        <li className="flex items-center space-x-3">
+                                            {/* <!-- Icon --> */}
+                                            <FaRegCheckCircle className="text-green-600 text-2xl"/>
+                                            <span>20 new content pages per month</span>
+                                        </li>
+                                        <li className="flex items-center space-x-3">
+                                            {/* <!-- Icon --> */}
+                                            <FaRegCheckCircle className="text-green-600 text-2xl"/>
+                                            <span>Personalized to your business</span>
+                                        </li>
+                                        <li className="flex items-center space-x-3">
+                                            {/* <!-- Icon --> */}
+                                            <FaRegCheckCircle className="text-green-600 text-2xl"/>
+                                            <span>High Quality Mixed Content</span>
+                                        </li>
+                                        <li className="flex items-center space-x-3">
+                                            {/* <!-- Icon --> */}
+                                            <FaRegCheckCircle className="text-green-600 text-2xl"/>
+                                            <span>SEO Optimized</span>
+                                        </li>
+                                        <li className="flex items-center space-x-3">
+                                            {/* <!-- Icon --> */}
+                                            <FaRegCheckCircle className="text-green-600 text-2xl"/>
+                                            <span>​Instant Indexing with Google & Bing</span>
+                                        </li>
+                                        <li className="flex items-center space-x-3">
+                                            {/* <!-- Icon --> */}
+                                            <FaRegCheckCircle className="text-green-600 text-2xl"/>
+                                            <span>Full Editing of all Blogs</span>
+                                        </li>
+                                        <li className="flex items-center space-x-3">
+                                            {/* <!-- Icon --> */}
+                                            <FaRegCheckCircle className="text-green-600 text-2xl"/>
+                                            <span>Internal Page Links</span>
+                                        </li>
+                                        <li className="flex items-center space-x-3">
+                                            {/* <!-- Icon --> */}
+                                            <FaRegCheckCircle className="text-green-600 text-2xl"/>
+                                            <span>​Call to Action (CTA) to any URL</span>
+                                        </li>
+                                    </ul>
+                                    <a href="#" className="text-white bg-blue-400 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center flex gap-2 justify-center">Start Free Trial <FaArrowRight className="mt-1"/></a>
+                                </div>
+                       
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
         </div>
     );
 };
