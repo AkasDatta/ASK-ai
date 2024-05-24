1efaaee0e89208ddd6bd24650e3440bbeb8ac17b
Akas_Datta <012akasdatta@gmail.com>
2024-05-24 23:45:19 +0600

    done video file

--- src/Pages/Home/Home/Home/Banner/BannerDown/BannerDown.jsx
+++ src/Pages/Home/Home/Home/Banner/BannerDown/BannerDown.jsx
@@ -1,6 +1,21 @@
 import './BannerDown.css';
-import simpleVideo from "../../../../../../assets/Banner/samplevideo.mp4"
+import simpleVideo from "../../../../../../assets/Banner/samplevideo.mp4";
+import { useRef, useState } from 'react';
+import { FaPlayCircle } from 'react-icons/fa';
+
 const BannerDown = () => {
+    const videoRef = useRef(null);
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    const handlePlayClick = () => {
+        if (videoRef.current) {
+            videoRef.current.play();
+            setIsPlaying(true);
+        }
+    };
+
+
+
     return (
         <div className='relative pb-0 lg:pb-72 xl:pb-80'>
             <div className="bg-container text-center rounded-lg">
@@ -17,10 +32,33 @@ const BannerDown = () => {
                         <textarea id="message" rows="5" className="block p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Search in PDF..."></textarea>
                     </div>
                 </div>
-                <div className='mt-8'>
-                    <video src={simpleVideo} className='mx-auto p-5' width="1080" height="720" controls></video>
+                <div className='mt-8 p-5'>
+                    <div className='relative border-4 2xl:mx-48 xl:mx-40 border-[#9fa1fe] rounded-xl shadow-2xl'>
+                        <video
+                            ref={videoRef}
+                            src={simpleVideo}
+                            width='1080px'
+                            height='720px'
+                            className='mx-auto rounded-lg'
+                            onClick={() => {
+                                if (isPlaying) {
+                                    videoRef.current.pause();
+                                    setIsPlaying(false);
+                                } else {
+                                    handlePlayClick();
+                                }
+                            }}
+                        ></video>
+                        {!isPlaying && (
+                            <div
+                                className='absolute inset-0 flex items-center justify-center cursor-pointer'
+                                onClick={handlePlayClick}
+                            >
+                                <FaPlayCircle className='text-3xl md:text-6xl lg:text-6xl text-[#B042E3] bg-white rounded-full m-0 p-0' />
+                            </div>
+                        )}
+                    </div>
                 </div>
-                
             </div>   
         </div>
     );
