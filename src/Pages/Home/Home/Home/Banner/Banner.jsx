ee742dfb19a1f649af958fd48aa0174e279cdc1c
Akas_Datta <012akasdatta@gmail.com>
2024-05-24 22:32:13 +0600

    video added and responsive done

--- src/Pages/Home/Home/Home/Banner/Banner.jsx
+++ src/Pages/Home/Home/Home/Banner/Banner.jsx
@@ -2,11 +2,11 @@ import BannerDown from "./BannerDown/BannerDown";
 
 const Banner = () => {
     return (
-        <div className="px-5 py-4 max-w-screen-2xl mx-auto lg:px-10 overflow-x-hidden">
+        <div className="px-5 py-4 max-w-screen-2xl mx-auto lg:px-10 overflow-x-hidden pb-5 md:pb-96 lg:pb-72 xl:pb-80">
             <div className="text-center pt-6 md:pt-20 lg:pt-24">
                 <h2 className="text-2xl md:text-5xl lg:text-6xl font-bold text-gray-800">Ask questions <br /></h2>
             </div>
-            <BannerDown></BannerDown>
+            <BannerDown className="pb-0"></BannerDown>
         </div>
     );
 };
--- src/Pages/Home/Home/Home/Banner/BannerDown/BannerDown.jsx
+++ src/Pages/Home/Home/Home/Banner/BannerDown/BannerDown.jsx
@@ -1,8 +1,8 @@
 import './BannerDown.css';
-
+import simpleVideo from "../../../../../../assets/Banner/samplevideo.mp4"
 const BannerDown = () => {
     return (
-        <div className='relative'>
+        <div className='relative pb-0 lg:pb-72 xl:pb-80'>
             <div className="bg-container text-center rounded-lg">
                 <div className='px-2 md:px-0 lg:px-0'>
                     <h2 className="text-2xl md:text-5xl lg:text-6xl font-bold text-gray-800"> from <span className="text-blue-500">any document</span></h2>
@@ -16,6 +16,9 @@ const BannerDown = () => {
                     <div className='border bg-[#F5FBFF] max-w-screen-sm mx-auto p-5 rounded-lg'>
                         <textarea id="message" rows="5" className="block p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Search in PDF..."></textarea>
                     </div>
+                </div>
+                <div className='mt-8'>
+                    <video src={simpleVideo} className='mx-auto p-5' width="1080" height="720" controls></video>
                 </div>
                 
             </div>   
