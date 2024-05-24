378226d57a56aa28c3762bfb73582f95cee14f37
Akas_Datta <012akasdatta@gmail.com>
2024-05-25 02:23:45 +0600

    project bug fixed

--- src/Pages/Home/Home/Home/Accordion/Accordionn.jsx
+++ src/Pages/Home/Home/Home/Accordion/Accordionn.jsx
@@ -1,8 +1,9 @@
 import { FaArrowRight } from "react-icons/fa";
 
 const Accordionn = () => {
-    return (
-        <div className="bg-[#F9FAFB] px-5 max-w-screen-2xl mx-auto lg:px-10 overflow-x-hidden py-10 md:py-20 lg:py-24 text-center">
+  return (
+
+    <div className="bg-[#F9FAFB] px-5 max-w-screen-2xl mx-auto lg:px-10 overflow-x-hidden py-10 md:py-20 lg:py-24 text-center">
             <div className='flex justify-center py-4 md:py-6 lg:pb-6'>
                 <p className="px-5 py-2 text-xs md:text-xl lg:text-xl text-[#344054] font-semibold bg-blue-50 rounded-full">
                     Have Questions About Pricing?
@@ -18,45 +19,92 @@ const Accordionn = () => {
             </div>
 
             <div className="py-10 max-w-screen-md mx-auto">
-                <div className="collapse collapse-plus bg-white my-4 border focus:border focus:border-blue-700 rounded-lg" tabIndex="0" onClick={(e) => e.currentTarget.focus()}>
-                    <input type="radio" name="my-accordion-3" /> 
-                    <div className="collapse-title text-lg font-bold text-left px-5 text-black">
-                        Is there a 30-day money back guarantee?
-                    </div>
-                    <div className="collapse-content text-left text-black"> 
-                        <p className="p-2">hello</p>
-                    </div>
-                </div>
-                <div className="collapse collapse-plus bg-white my-4 border focus:border focus:border-blue-700 rounded-lg" tabIndex="0" onClick={(e) => e.currentTarget.focus()}>
-                    <input type="radio" name="my-accordion-3" /> 
-                    <div className="collapse-title text-lg font-bold text-left px-5 text-black">
-                        Can I change my plan later on?
-                    </div>
-                    <div className="collapse-content text-left text-black"> 
-                        <p className="p-2">hello</p>
-                    </div>
-                </div>
-                <div className="collapse collapse-plus bg-white my-4 border focus:border focus:border-blue-700 rounded-lg" tabIndex="0" onClick={(e) => e.currentTarget.focus()}>
-                    <input type="radio" name="my-accordion-3" defaultChecked /> 
-                    <div className="collapse-title text-lg font-bold text-left px-5 text-black">
-                        Can I use a custom domain?
-                    </div>
-                    <div className="collapse-content text-left text-black"> 
-                        <p className="p-2">Yes. You can use your custom domain when you have “Traffic Monster” Plan.</p>
-                    </div>
-                </div>
-                <div className="collapse collapse-plus bg-white my-4 border focus:border focus:border-blue-700 rounded-lg" tabIndex="0" onClick={(e) => e.currentTarget.focus()}>
-                    <input type="radio" name="my-accordion-3" /> 
-                    <div className="collapse-title text-lg font-bold text-left px-5 text-black">
-                        What are Ask AI?
-                    </div>
-                    <div className="collapse-content text-left text-black"> 
-                        <p className="p-2">hello</p>
-                    </div>
-                </div>
+
+                <details className="w-full bg-white my-4 border hover:border-blue-700 rounded-lg">
+                    <summary className="w-full bg-white text-dark flex justify-between  py-3  text-lg font-bold text-left px-5 text-black after:content-['+'] rounded-lg">Is there a 30-day money back guarantee?</summary>
+                    <p className=" pb-4 px-5">
+                        Hello
+                    </p>
+                </details>
+                <details className="w-full bg-white my-4 border hover:border-blue-700 rounded-lg">
+                    <summary className="w-full bg-white text-dark flex justify-between  py-3  text-lg font-bold text-left px-5 text-black after:content-['+'] rounded-lg">Can I change my plan later on?</summary>
+                    <p className=" pb-4 px-5">
+                        Hello
+                    </p>
+                </details>
+                <details className="w-full bg-white my-4 border hover:border-blue-700 rounded-lg">
+                    <summary className="w-full bg-white text-dark flex justify-between  py-3  text-lg font-bold text-left px-5 text-black after:content-['+'] rounded-lg">Can I use a custom domain?</summary>
+                    <p className=" pb-4 px-5">
+                        Yes. You can use your custom domain when you have “Traffic Monster” Plan.
+                    </p>
+                </details>
+                <details className="w-full bg-white my-4 border hover:border-blue-700 rounded-lg">
+                    <summary className="w-full bg-white text-dark flex justify-between  py-3  text-lg font-bold text-left px-5 text-black after:content-['+'] rounded-lg"> What are Ask AI?</summary>
+                    <p className=" pb-4 px-5">
+                        Hello
+                    </p>
+                </details>
+                        
+                <style>
+                    {`
+                        details summary::-webkit-details-marker {
+                            display: none;
+                        }
+
+                        details[open] summary {
+                            background: white;
+                            color: black;
+                        }
+
+                        details[open] summary::after {
+                            content: "-";
+                        }
+
+                        details[open] summary ~ * {
+                            animation: slideDown 0.3s ease-in-out;
+                            text-align: left;
+                            color:black;
+                        }
+
+                        details[open] summary p {
+                            opacity: 0;
+                            animation-name: showContent;
+                            animation-duration: 0.6s;
+                            animation-delay: 0.2s;
+                            animation-fill-mode: forwards;
+                            margin: 0;
+                        }
+
+                        @keyframes showContent {
+                            from {
+                                opacity: 0;
+                                height: 0;
+                                }
+                                to {
+                                opacity: 1;
+                                height: auto;
+                                }
+                            }
+
+                        @keyframes slideDown {
+                            from {
+                                opacity: 0;
+                                height: 0;
+                                padding: 0;
+                                }
+
+                            to {
+                                opacity: 1;
+                                height: auto;
+                                }
+                        }
+                    `}
+                </style>
             </div>
-        </div>
-    );
+    </div>
+  
+    
+  );
 };
 
 export default Accordionn;
--- src/Pages/Home/Home/Home/Banner/Banner.jsx
+++ src/Pages/Home/Home/Home/Banner/Banner.jsx
@@ -12,3 +12,6 @@ const Banner = () => {
 };
 
 export default Banner;
+
+
+ 
